a3a6ccf2ffe0c67debcfec220f8a1bb4f45d0e48
Ousmane Diallo <ousmanediallo@Ousmanes-MBP.lan>
2023-04-12 12:10:34 -0400

    added everything

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("1: Apple");
+        System.out.println("2: Banana");
+        System.out.println("3: Lemon");
+        System.out.println("4: Watermelon");
+        System.out.println("5: Mango");
+        System.out.println("5: Potato");
+        System.out.println("6: Lettuce");
+        System.out.println("7: Rice");
+        System.out.println("8: Eggs");
+        System.out.println("9: Tomatoes");
+        System.out.println("10: Milk");
+    }
+}
